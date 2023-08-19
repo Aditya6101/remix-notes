bb15ae9ca005b3d9e9e88c0ba11c4095d768d891
Aditya Kamble <adityakamble6101@gmail.com>
2023-08-19 22:38:25 +0530

    remix-indie-stack init && add edit note feature

---
+++ app/routes/notes.edit.$noteId.tsx
@@ -0,0 +1,192 @@
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  Form,
+  isRouteErrorResponse,
+  useActionData,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import { useEffect, useRef } from "react";
+import invariant from "tiny-invariant";
+
+import { editNote, deleteNote, getNote } from "~/models/note.server";
+import { requireUserId } from "~/session.server";
+
+export const loader = async ({ params, request }: LoaderArgs) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  const note = await getNote({ id: params.noteId, userId });
+  if (!note) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ note });
+};
+
+export const action = async (args: ActionArgs) => {
+  const formData = await args.request.clone().formData();
+  const actionType = formData.get("_action");
+
+  switch (actionType) {
+    case "update":
+      return editAction(args);
+    case "delete":
+      return deleteAction(args);
+
+    default:
+      return json(
+        { errors: { formError: "Something went wrong!" } },
+        { status: 400 }
+      );
+  }
+};
+
+const deleteAction = async ({ params, request }: ActionArgs) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  await deleteNote({ id: params.noteId, userId });
+  return redirect("/notes");
+};
+
+const editAction = async ({ params, request }: ActionArgs) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const body = formData.get("body");
+  invariant(params.noteId, "noteId not found");
+
+  if (typeof title !== "string" || title.length === 0) {
+    return json(
+      { errors: { body: null, title: "Title is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (typeof body !== "string" || body.length === 0) {
+    return json(
+      { errors: { body: "Body is required", title: null } },
+      { status: 400 }
+    );
+  }
+
+  const note = await editNote({ id: params.noteId, body, title });
+
+  return redirect(`/notes/${note.id}`);
+};
+
+export default function NoteDetailsPage() {
+  const data = useLoaderData<typeof loader>();
+
+  const actionData = useActionData<typeof action>();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (action?.errors?.title) {
+      titleRef.current?.focus();
+    } else if (actionData?.errors?.body) {
+      bodyRef.current?.focus();
+    }
+  }, [actionData]);
+
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">Edit - {data.note.title}</h3>
+      <Form
+        method="post"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+          width: "100%",
+        }}
+      >
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Title: </span>
+            <input
+              ref={titleRef}
+              name="title"
+              defaultValue={data.note.title}
+              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+              aria-invalid={actionData?.errors?.title ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.title ? "title-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.errors?.title ? (
+            <div className="pt-1 text-red-700" id="title-error">
+              {actionData.errors.title}
+            </div>
+          ) : null}
+        </div>
+
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Body: </span>
+            <textarea
+              ref={bodyRef}
+              name="body"
+              rows={8}
+              defaultValue={data.note.body}
+              className="w-full flex-1 rounded-md border-2 border-blue-500 px-3 py-2 text-lg leading-6"
+              aria-invalid={actionData?.errors?.body ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.body ? "body-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.errors?.body ? (
+            <div className="pt-1 text-red-700" id="body-error">
+              {actionData.errors.body}
+            </div>
+          ) : null}
+        </div>
+
+        <div className="text-right">
+          <button
+            type="submit"
+            name="_action"
+            value="update"
+            className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+          >
+            Save
+          </button>
+        </div>
+      </Form>
+
+      <hr className="my-4" />
+
+      <Form method="post">
+        <button
+          type="submit"
+          name="_action"
+          value="delete"
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Delete
+        </button>
+      </Form>
+    </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (error instanceof Error) {
+    return <div>An unexpected error occurred: {error.message}</div>;
+  }
+
+  if (!isRouteErrorResponse(error)) {
+    return <h1>Unknown Error</h1>;
+  }
+
+  if (error.status === 404) {
+    return <div>Note not found</div>;
+  }
+
+  return <div>An unexpected error occurred: {error.statusText}</div>;
+}
