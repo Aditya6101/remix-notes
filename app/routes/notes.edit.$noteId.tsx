277b51a0d0a7e6d0c6d45d262a6fb566c405cede
Aditya Kamble <adityakamble6101@gmail.com>
2023-11-22 23:15:37 +0530

    add loading UI to edit note route

--- app/routes/notes.edit.$noteId.tsx
+++ app/routes/notes.edit.$noteId.tsx
@@ -6,12 +6,14 @@ import {
   isRouteErrorResponse,
   useActionData,
   useLoaderData,
+  useNavigation,
   useRouteError,
 } from "@remix-run/react";
 import { useEffect, useRef } from "react";
 import invariant from "tiny-invariant";
+import { delay } from "utils";
 
-import { editNote, deleteNote, getNote } from "~/models/note.server";
+import { deleteNote, editNote, getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 
 export const loader = async ({ params, request }: LoaderArgs) => {
@@ -27,14 +29,15 @@ export const loader = async ({ params, request }: LoaderArgs) => {
 
 export const action = async (args: ActionArgs) => {
   const formData = await args.request.clone().formData();
-  const actionType = formData.get("_action");
+  const intent = formData.get("intent");
 
-  switch (actionType) {
+  await delay(3000);
+
+  switch (intent) {
     case "update":
-      return editAction(args);
+      return updateAction(args);
     case "delete":
       return deleteAction(args);
-
     default:
       return json(
         { errors: { formError: "Something went wrong!" } },
@@ -51,7 +54,7 @@ const deleteAction = async ({ params, request }: ActionArgs) => {
   return redirect("/notes");
 };
 
-const editAction = async ({ params, request }: ActionArgs) => {
+const updateAction = async ({ params, request }: ActionArgs) => {
   const formData = await request.formData();
   const title = formData.get("title");
   const body = formData.get("body");
@@ -79,9 +82,14 @@ const editAction = async ({ params, request }: ActionArgs) => {
 export default function NoteDetailsPage() {
   const data = useLoaderData<typeof loader>();
 
-  const actionData = useActionData<typeof editAction>();
+  const actionData = useActionData<typeof updateAction>();
   const titleRef = useRef<HTMLInputElement>(null);
   const bodyRef = useRef<HTMLTextAreaElement>(null);
+
+  const navigation = useNavigation();
+
+  const isUpdating = navigation.formData?.get("intent") === "update";
+  const isDeleting = navigation.formData?.get("intent") === "delete";
 
   useEffect(() => {
     if (actionData?.errors?.title) {
@@ -94,65 +102,109 @@ export default function NoteDetailsPage() {
   return (
     <div>
       <h3 className="text-2xl font-bold">Edit - {data.note.title}</h3>
-      <Form
-        method="post"
-        className="mx-auto flex w-1/2 flex-col gap-4 rounded-md border border-neutral-100 p-6 shadow-sm"
-      >
-        <div>
-          <TextField.Input
-            ref={titleRef}
-            name="title"
-            defaultValue={data.note.title}
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-            aria-invalid={actionData?.errors?.title ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.title ? "title-error" : undefined
-            }
-          />
+      <Form method="post" className="mx-auto w-1/2 rounded-md bg-neutral-100">
+        <fieldset
+          disabled={isUpdating}
+          className="mx-auto flex w-full flex-col gap-4 rounded-md bg-neutral-100 p-8 shadow-sm"
+        >
+          <div>
+            <TextField.Input
+              ref={titleRef}
+              name="title"
+              defaultValue={data.note.title}
+              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+              aria-invalid={actionData?.errors?.title ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.title ? "title-error" : undefined
+              }
+            />
 
-          {actionData?.errors?.title ? (
-            <div className="pt-1 text-red-700" id="title-error">
-              {actionData.errors.title}
-            </div>
-          ) : null}
-        </div>
+            {actionData?.errors?.title ? (
+              <div className="pt-1 text-red-700" id="title-error">
+                {actionData.errors.title}
+              </div>
+            ) : null}
+          </div>
 
-        <div>
-          <TextArea
-            ref={bodyRef}
-            name="body"
-            rows={8}
-            defaultValue={data.note.body}
-            className="w-full flex-1 rounded-md border-2 border-blue-500 px-3 py-2 text-lg leading-6"
-            aria-invalid={actionData?.errors?.body ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.body ? "body-error" : undefined
-            }
-          />
+          <div>
+            <TextArea
+              ref={bodyRef}
+              name="body"
+              rows={8}
+              defaultValue={data.note.body}
+              className="w-full flex-1 rounded-md border-2 border-blue-500 px-3 py-2 text-lg leading-6"
+              aria-invalid={actionData?.errors?.body ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.body ? "body-error" : undefined
+              }
+            />
 
-          {actionData?.errors?.body ? (
-            <div className="pt-1 text-red-700" id="body-error">
-              {actionData.errors.body}
-            </div>
-          ) : null}
-        </div>
+            {actionData?.errors?.body ? (
+              <div className="pt-1 text-red-700" id="body-error">
+                {actionData.errors.body}
+              </div>
+            ) : null}
+          </div>
 
-        <Button type="submit" name="_action" value="update">
-          Save
-        </Button>
+          <Button type="submit" name="intent" value="update">
+            {isUpdating ? (
+              <>
+                <svg
+                  aria-hidden="true"
+                  className="inline h-4 w-4 animate-spin fill-blue-500 text-white"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>{" "}
+                Saving...
+              </>
+            ) : (
+              "Save"
+            )}
+          </Button>
 
-        <Form method="post">
           <Button
             variant="soft"
             color="red"
             type="submit"
-            name="_action"
+            name="intent"
             value="delete"
             className="w-full"
           >
-            Delete
+            {isDeleting ? (
+              <>
+                <svg
+                  aria-hidden="true"
+                  className="inline h-4 w-4 animate-spin fill-red-500 text-white"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>{" "}
+                Deleting...
+              </>
+            ) : (
+              "Delete"
+            )}
           </Button>
-        </Form>
+        </fieldset>
       </Form>
     </div>
   );
